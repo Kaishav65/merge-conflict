90f7f2df15e72d1989ce242bda92e8e218088a25
kaishav basodiya <kaishavbasodiya@gmail.com>
2023-03-15 15:03:54 +0530

    kaishav

--- main.c
+++ main.c
@@ -3,11 +3,13 @@ pseudo code
 Initial Function()
 {
   //Sample Code
+  This Initial funcion is changed by kaishav
 }
 
 Initial Security()
 {
  //Sample Code
+ this initial security function is changed
 }
 
 
